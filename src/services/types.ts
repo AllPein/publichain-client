00c4ca237f79bc2312ed85b4ebfc4fc5d2c97178
apanin <aleksandr.panin@tedo.ru>
2023-03-02 22:52:32 +0500

    added mint button for test

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,9 +1,53 @@
+import {
+  AxiosRequestConfig,
+  AxiosResponse,
+  RawAxiosRequestHeaders,
+} from 'axios';
+
 import { AccountInfo } from '@/store/StoreTypes';
 import { XummPkce } from '@/utils/window';
+
+export interface AxiosClient {
+  init: (baseUrl: string) => void;
+  get: <T>(
+    url: string,
+    params?: object,
+    options?: AxiosRequestConfig,
+  ) => Promise<AxiosResponse<T, unknown>>;
+  post: <T, K>(
+    url: string,
+    data: K,
+    headers?: RawAxiosRequestHeaders,
+    params?: object,
+  ) => Promise<AxiosResponse<T, unknown>>;
+  put: <T, K>(
+    url: string,
+    data: K,
+    headers?: RawAxiosRequestHeaders,
+  ) => Promise<AxiosResponse<T, unknown>>;
+  patch: <T, K>(
+    url: string,
+    data: K,
+    headers?: RawAxiosRequestHeaders,
+    params?: object,
+  ) => Promise<AxiosResponse<T, unknown>>;
+  delete: <T, K>(
+    url: string,
+    data: K,
+    headers?: RawAxiosRequestHeaders,
+  ) => Promise<AxiosResponse<T, unknown>>;
+  sendFormData: <T>(
+    url: string,
+    data: FormData,
+    params?: object,
+  ) => Promise<AxiosResponse<T, unknown>>;
+}
 
 export interface IProjectService {
   client: any;
   xumm: typeof XummPkce;
+  axiosClient: AxiosClient;
 
+  mint(wallet): Promise<any>;
   login(): Promise<AccountInfo>;
 }
